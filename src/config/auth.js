efb986b8fca8abdadaefe18efdf4fb3d5a5642f5
Wellington Reis <wellington.costa.97@gmail.com>
2020-04-19 23:40:09 -0300

    Backend almost complete

---
+++ src/config/auth.js
@@ -0,0 +1,4 @@
+export default {
+    secret: '50303c908cd3fe95568b58f66d12e793',
+    expiresIn: '2d'
+}
---
+++ src/config/mail.js
@@ -0,0 +1,15 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: '7db1ee1a75fc70',
+    pass: '93a9bad374b8d0',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com.br',
+  },
+};
+
+// Use for DEV Mailtrap
+// Use for production Amazon SES or Mailgun
